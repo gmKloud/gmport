3503727fa2a60e635956c507b4ac0be172ba263a
grinil khanna <grinil_khanna@cloudalyze.net>
2017-05-12 11:10:38 -0700

    WIll add action links to contact and about with those page templates and will start focusing on API call

--- src/gmport/Controllers/ContactController.cs
+++ src/gmport/Controllers/ContactController.cs
@@ -4,17 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace gmport.Controllers
 {
-    public class HomeController : Controller
+    public class ContactController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
-
     }
 }
